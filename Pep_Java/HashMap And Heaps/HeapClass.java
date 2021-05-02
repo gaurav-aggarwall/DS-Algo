3a947af6fc5dbf162ec0aab103b57496a50bacad
gaurav-aggarwall <mail.gauravagg@gmail.com>
2021-05-02 18:09:02 +0530

    Heap completed

--- Pep_Java/HashMap And Heaps/HeapClass.java
+++ Pep_Java/HashMap And Heaps/HeapClass.java
@@ -43,6 +43,46 @@ public class HeapClass {
     }
   }
 
+  // Merge K Sorted
+  public static class Pair implements Comparable<Pair> {
+    int value;
+    int list;
+    int index;
+
+    Pair(int value, int list, int index) {
+      this.value = value;
+      this.list = list;
+      this.index = index;
+    }
+
+    public int compareTo(Pair o) {
+      return this.value - o.value;
+    }
+  }
+
+  public static ArrayList<Integer> mergeKSortedLists(ArrayList<ArrayList<Integer>> lists) {
+    ArrayList<Integer> rv = new ArrayList<>();
+
+    PriorityQueue<Pair> pq = new PriorityQueue<Pair>();
+
+    for (int i = 0; i < lists.size(); i++) {
+      pq.add(new Pair(lists.get(i).get(0), i, 0));
+    }
+
+    while (pq.size() > 0) {
+      Pair pair = pq.remove();
+      rv.add(pair.value);
+      pair.index++;
+
+      if (pair.index < lists.get(pair.list).size()) {
+        pair.value = lists.get(pair.list).get(pair.index);
+        pq.add(pair);
+      }
+    }
+
+    return rv;
+  }
+
   public static void main(String[] args) throws Exception {
     // Too lazy to handle inut and call functions :p
   }
