dee51aad8fec0d3fea723701d0ab2b9577cff75f
gaurav-aggarwall <mail.gauravagg@gmail.com>
2021-05-08 18:40:41 +0530

    graph continued

--- Pep_Java/Graphs/Graph.java
+++ Pep_Java/Graphs/Graph.java
@@ -171,6 +171,7 @@ public class Graph {
     return islands;
   }
 
+  // Visit Island
   public static void visitIsland(int[][] arr, int i, int j, boolean[][] visited) {
     if (i < 0 || i >= arr.length || j < 0 || j >= arr[0].length || arr[i][j] == 1 || visited[i][j] == true)
       return;
@@ -181,6 +182,102 @@ public class Graph {
     visitIsland(arr, i, j + 1, visited);
     visitIsland(arr, i + 1, j, visited);
     visitIsland(arr, i, j - 1, visited);
+  }
+
+  // Hamiltonaian Path / Cycle
+  // If set has all nodes meaning we are on last node and it has original source
+  // as neighnour thrn cylce otherwise path
+  public static void hamiltonian(ArrayList<Edge>[] graph, int src, HashSet<Integer> visited, String psf, int osrc) {
+    if (visited.size() == graph.length - 1) {
+      boolean cycle = false;
+      for (Edge edge : graph[src]) {
+        if (edge.nbr == osrc) {
+          cycle = true;
+          break;
+        }
+      }
+      if (cycle) {
+        psf += "*";
+      } else {
+        psf += ".";
+      }
+      System.out.println(psf);
+      return;
+    }
+
+    visited.add(src);
+    for (Edge edge : graph[src]) {
+      if (visited.contains(edge.nbr) == false) {
+        hamiltonian(graph, edge.nbr, visited, psf + edge.nbr, osrc);
+      }
+    }
+    visited.remove(src);
+
+  }
+
+  // BFS
+  static class BFSPair {
+    int v;
+    String psf;
+
+    BFSPair(int v, String psf) {
+      this.v = v;
+      this.psf = psf;
+    }
+  }
+
+  public static void bfs(ArrayList<Edge>[] graph, int src, boolean[] visited) {
+    ArrayDeque<BFSPair> q = new ArrayDeque<BFSPair>();
+    q.add(new BFSPair(src, src + ""));
+    visited[src] = true;
+
+    while (q.size() > 0) {
+      int size = q.size();
+
+      for (int i = 0; i < size; i++) {
+        BFSPair pair = q.remove();
+        System.out.println(pair.v + "@" + pair.psf);
+        for (Edge edge : graph[pair.v]) {
+          if (visited[edge.nbr] == false) {
+            visited[edge.nbr] = true;
+            q.add(new BFSPair(edge.nbr, pair.psf + edge.nbr));
+          }
+        }
+      }
+    }
+  }
+
+  // Is Cyclic
+  public static void isCyclic(ArrayList<Edge>[] graph, boolean[] visited) {
+    for (int v = 0; v < graph.length; v++) {
+      if (visited[v] == false) {
+        boolean cycle = isCyclicHelper(graph, v, visited);
+        if (cycle) {
+          System.out.println(true);
+          return;
+        }
+      }
+    }
+    System.out.println(false);
+  }
+
+  public static boolean isCyclicHelper(ArrayList<Edge>[] graph, int src, boolean[] visited) {
+    ArrayDeque<BFSPair> q = new ArrayDeque<BFSPair>();
+    q.add(new BFSPair(src, src + ""));
+
+    while (q.size() > 0) {
+      BFSPair pair = q.remove();
+      if (visited[pair.v] == true) {
+        return true;
+      }
+      visited[pair.v] = true;
+      for (Edge edge : graph[pair.v]) {
+        if (visited[edge.nbr] == false) {
+          q.add(new BFSPair(edge.nbr, pair.psf + edge.nbr));
+        }
+      }
+    }
+    return false;
   }
 
   public static void main(String[] args) throws Exception {
