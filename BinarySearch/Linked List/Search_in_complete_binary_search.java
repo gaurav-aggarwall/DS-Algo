3207362e25d1c955ac2a4b1a7367d2e318619b3b
gaurav-aggarwall <mail.gauravagg@gmail.com>
2021-05-11 21:39:59 +0530

    Binary Search Practice started

---
+++ BinarySearch/Linked List/Search_in_complete_binary_search.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+/**
+ * public class Tree { int val; Tree left; Tree right; }
+ */
+class Search_in_complete_binary_search {
+  public boolean solve(Tree root, int target) {
+    if (root == null)
+      return false;
+
+    if (root.val == target)
+      return true;
+
+    boolean gr = goRight(root.val, target);
+
+    if (gr)
+      return solve(root.right, target);
+    else
+      return solve(root.left, target);
+  }
+
+  public static boolean goRight(int val, int target) {
+    int left = val * 2;
+    int right = left + 1;
+
+    while (target > right) {
+      target /= 2;
+    }
+    return target == right;
+  }
+
+}
---
+++ BinarySearch/Linked List/Vertical_Lines.java
@@ -0,0 +1,34 @@
+// Approach -> Ans is width of tree.
+//  when going left pass level - 1
+// else in right pass level + 1
+//  and update what is the max left and right we have traversed and as left is negative value ans is right - left + 1. 
+// (1 is for root which is at 0 of number line)
+
+import java.util.*;
+
+/**
+ * public class Tree {
+ *   int val;
+ *   Tree left;
+ *   Tree right;
+ * }
+ */
+class Solution {
+    int left = 0;
+    int right = 0;
+
+    public int solve(Tree root) {
+        helper(root, 0);
+        return right - left + 1;
+    }
+    
+    void helper(Tree root, int level){
+        if(root == null) return;
+        
+        if(level < left) left = level;
+        if(level > right) right = level;
+
+        helper(root.left, level-1);
+        helper(root.right,level+1);
+    }
+}
